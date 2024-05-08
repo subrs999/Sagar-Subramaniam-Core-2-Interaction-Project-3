c44fe724c4d7405d0f297dc7c4e2a529e7d1249d
subrs999 <146101715+subrs999@users.noreply.github.com>
2024-05-08 13:23:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,501 @@
+let data = [
+    {
+      "_zone": "Brooklyn North",
+      "district": "BKN01",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "14.6870926033314",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "44.1467643388238"
+    },
+    {
+      "_zone": "Brooklyn North",
+      "district": "BKN02",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "19.9501814568266",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "41.2136999269730"
+    },
+    {
+      "_zone": "Brooklyn North",
+      "district": "BKN03",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "12.1641613296565",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "38.1559365633997"
+    },
+    {
+      "_zone": "Brooklyn North",
+      "district": "BKN04",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "15.5418031271589",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "48.7507554470644"
+    },
+    {
+      "_zone": "Brooklyn North",
+      "district": "BKN05",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "10.0518454148520",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "31.5301290076712"
+    },
+    {
+      "_zone": "Brooklyn North",
+      "district": "BKN08",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "16.4483838242055",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "50.5504139390847"
+    },
+    {
+      "_zone": "Brooklyn North",
+      "district": "BKN09",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "13.9656800914806",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "39.2362901346726"
+    },
+    {
+      "_zone": "Brooklyn North",
+      "district": "BKN16",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "9.3035157873820",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "26.3709407757381"
+    },
+    {
+      "_zone": "Brooklyn North",
+      "district": "BKN17",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "13.3531840029145",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "40.7187278494377"
+    },
+    {
+      "_zone": "Brooklyn South",
+      "district": "BKS06",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "28.9512974587036",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "57.2441131708991"
+    },
+    {
+      "_zone": "Brooklyn South",
+      "district": "BKS07",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "20.3784834210320",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "57.3069985937605"
+    },
+    {
+      "_zone": "Brooklyn South",
+      "district": "BKS10",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "23.7688290120229",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "61.2682730526777"
+    },
+    {
+      "_zone": "Brooklyn South",
+      "district": "BKS11",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "18.3833471388244",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "52.5087725340162"
+    },
+    {
+      "_zone": "Brooklyn South",
+      "district": "BKS12",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "14.9388304014584",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "45.3228682777457"
+    },
+    {
+      "_zone": "Brooklyn South",
+      "district": "BKS13",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "14.3481741742713",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "45.5607691296216"
+    },
+    {
+      "_zone": "Brooklyn South",
+      "district": "BKS14",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "14.9961678861179",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "45.1165255200942"
+    },
+    {
+      "_zone": "Brooklyn South",
+      "district": "BKS15",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "18.6553042325098",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "52.8728175857441"
+    },
+    {
+      "_zone": "Brooklyn South",
+      "district": "BKS18",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "14.7319554977648",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "50.3033786294573"
+    },
+    {
+      "_zone": "Bronx",
+      "district": "BX01",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "7.1464990151248",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "23.2398996272091"
+    },
+    {
+      "_zone": "Bronx",
+      "district": "BX02",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "13.3878358065643",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "44.9649809228277"
+    },
+    {
+      "_zone": "Bronx",
+      "district": "BX03",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "9.8046733167751",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "32.9304119809407"
+    },
+    {
+      "_zone": "Bronx",
+      "district": "BX04",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "12.2530409793966",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "40.1584429636750"
+    },
+    {
+      "_zone": "Bronx",
+      "district": "BX05",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "11.4152622851406",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "38.3398077401538"
+    },
+    {
+      "_zone": "Bronx",
+      "district": "BX06",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "14.1283180397505",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "43.1643549018265"
+    },
+    {
+      "_zone": "Bronx",
+      "district": "BX07",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "14.6308704394292",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "49.1398923394870"
+    },
+    {
+      "_zone": "Bronx",
+      "district": "BX08",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "21.1617848842312",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "57.5705543717200"
+    },
+    {
+      "_zone": "Bronx",
+      "district": "BX09",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "10.0878444009675",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "31.6430488377641"
+    },
+    {
+      "_zone": "Bronx",
+      "district": "BX10",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "20.9783161039449",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "55.8453260514362"
+    },
+    {
+      "_zone": "Bronx",
+      "district": "BX11",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "16.6594704269834",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "44.6399229306593"
+    },
+    {
+      "_zone": "Bronx",
+      "district": "BX12",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "20.5839049211805",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "52.3965185199716"
+    },
+    {
+      "_zone": "Manhattan",
+      "district": "MN01",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "28.0246416680049",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "54.0724968095967"
+    },
+    {
+      "_zone": "Manhattan",
+      "district": "MN02",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "26.7143806048852",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "51.5227715298576"
+    },
+    {
+      "_zone": "Manhattan",
+      "district": "MN03",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "14.1064074527328",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "42.6840511410294"
+    },
+    {
+      "_zone": "Manhattan",
+      "district": "MN04",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "23.5672366609756",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "44.9772464609239"
+    },
+    {
+      "_zone": "Manhattan",
+      "district": "MN05",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "21.1397076494414",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "42.0066050848924"
+    },
+    {
+      "_zone": "Manhattan",
+      "district": "MN06",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "24.0012405542628",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "45.9165033763141"
+    },
+    {
+      "_zone": "Manhattan",
+      "district": "MN07",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "27.5121894386379",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "52.6278964325395"
+    },
+    {
+      "_zone": "Manhattan",
+      "district": "MN08",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "26.7404554153102",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "52.7133113234877"
+    },
+    {
+      "_zone": "Manhattan",
+      "district": "MN09",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "18.5040758617699",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "54.7617705681879"
+    },
+    {
+      "_zone": "Manhattan",
+      "district": "MN10",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "14.6492145788832",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "48.2452106190766"
+    },
+    {
+      "_zone": "Manhattan",
+      "district": "MN11",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "11.3369989560303",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "36.8710452288693"
+    },
+    {
+      "_zone": "Manhattan",
+      "district": "MN12",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "16.3874734204534",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "51.7952523173508"
+    },
+    {
+      "_zone": "Queens East",
+      "district": "QE07",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "20.2270997203441",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "56.0034902209323"
+    },
+    {
+      "_zone": "Queens East",
+      "district": "QE08",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "16.5276222636613",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "36.1971402500735"
+    },
+    {
+      "_zone": "Queens East",
+      "district": "QE10",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "18.9774768988690",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "60.7976276703860"
+    },
+    {
+      "_zone": "Queens East",
+      "district": "QE11",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "24.1926878639499",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "72.7172969898576"
+    },
+    {
+      "_zone": "Queens East",
+      "district": "QE12",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "13.8666753825287",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "50.4540236201878"
+    },
+    {
+      "_zone": "Queens East",
+      "district": "QE13",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "20.0408248308452",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "58.2118465531820"
+    },
+    {
+      "_zone": "Queens East",
+      "district": "QE14",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "14.0026442106188",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "37.6754839769643"
+    },
+    {
+      "_zone": "Queens West",
+      "district": "QW01",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "19.7697363153652",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "60.2851359274913"
+    },
+    {
+      "_zone": "Queens West",
+      "district": "QW02",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "21.0434513048468",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "58.6205026199681"
+    },
+    {
+      "_zone": "Queens West",
+      "district": "QW03",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "13.8866353353841",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "42.3454155085766"
+    },
+    {
+      "_zone": "Queens West",
+      "district": "QW04",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "14.9432220939990",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "45.5673338663233"
+    },
+    {
+      "_zone": "Queens West",
+      "district": "QW05",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "23.2916081740347",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "61.2729328179252"
+    },
+    {
+      "_zone": "Queens West",
+      "district": "QW06",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "20.3674858275299",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "40.4721270472157"
+    },
+    {
+      "_zone": "Queens West",
+      "district": "QW09",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "21.8457404844755",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "54.5899514076172"
+    },
+    {
+      "_zone": "Staten Island",
+      "district": "SI01",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "20.1846373914586",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "59.5932691253666"
+    },
+    {
+      "_zone": "Staten Island",
+      "district": "SI02",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "19.3363072658364",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "63.4365855268637"
+    },
+    {
+      "_zone": "Staten Island",
+      "district": "SI03",
+      "fiscal_year": "2019",
+      "month_name": "April",
+      "diversion_rate_total_total_recycling_total_waste_": "19.5282416744231",
+      "capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100": "65.7897806384339"
+    }
+  ]
+
+  const init = () => {
+    const mapContainer = document.getElementById("blockMap");
+    const textContainer = document.getElementById("mapInfo");
+
+    mapContainer.addEventListener("mousemove", (ev) => {
+        let districtId = ev.target.id;
+        let districtData = data.find(item => item.district === districtId);
+        if (districtData) {
+            let hoverDistrict = districtData.district;
+            let hoverZone = districtData._zone;
+            let hoverDiversion = Math.round(districtData.diversion_rate_total_total_recycling_total_waste_ * 100) / 100;
+            let hoverCapture = Math.round(districtData.capture_rate_total_total_recycling_leaves_recycling_max_paper_max_mgp_x100 * 100) / 100;
+
+            textContainer.innerHTML =
+                `<h2>District: ${hoverDistrict}</h2>
+                <h2>Zone: ${hoverZone}</h2>
+                <h2>Diversion Rate: ${hoverDiversion}%</h2>
+                <h2>Capture Rate: ${hoverCapture}%</h2>`;
+        }
+    });
+}
+
+
+
+window.addEventListener("load", init)
+
